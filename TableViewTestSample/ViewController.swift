e7813d9a4f41a409913aa203abfc009a516e45e6
rhiramat <rhiramat@yahoo-corp.jp>
2018-03-27 15:22:48 +0900

    initial commit

--- TableViewTestSample/ViewController.swift
+++ TableViewTestSample/ViewController.swift
@@ -1,25 +1,54 @@
-//
-//  ViewController.swift
-//  TableViewTestSample
-//
-//  Created by 平松　亮介 on 2018/03/27.
-//  Copyright © 2018年 平松　亮介. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let tableView = UITableView()
+    var items = ["Hello", "World", "Swift 4", "Xcode 9.2", "iPhone X", "WWDC 2018"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.addSubview(tableView)
+        
+        // TableViewを画面いっぱいに貼る
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            ])
+        
+        tableView.dataSource = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
+
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2    // ヘッダー + コンテンツ
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 1
+        } else {
+            return items.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        if indexPath.section == 0 {
+            cell.textLabel?.text = "TableViewのテストのサンプルです"
+            cell.textLabel?.font = UIFont.systemFont(ofSize: 14.0)
+            cell.textLabel?.textColor = .gray
+            cell.textLabel?.textAlignment = .center
+        } else {
+            cell.textLabel?.text = items[indexPath.row]
+        }
+        return cell
+    }
+}
+
+
 
---
+++ TableViewTestSampleTests/TableViewTestSampleTests.swift
@@ -0,0 +1,47 @@
+import XCTest
+@testable import TableViewTestSample
+
+class TableViewTestSampleTests: XCTestCase {
+    
+    func test_tableViewが表示されること() {
+        let vc = ViewController()
+        XCTAssertNotNil(vc.tableView)
+        XCTAssertTrue(vc.view.subviews.contains(vc.tableView))
+    }
+    
+    func test_sectionが2つあること() {
+        let vc = ViewController()
+        
+        let sectionCount = vc.numberOfSections(in: vc.tableView)
+        XCTAssertEqual(sectionCount, 2)
+    }
+    
+    func test_1つ目のsectionにはセルが1つだけであること() {
+        let vc = ViewController()
+        
+        let rowCount = vc.tableView(vc.tableView, numberOfRowsInSection: 0)
+        XCTAssertEqual(rowCount, 1)
+    }
+
+    func test_2つ目のsectionには与えられたアイテムの数だけセルが表示されること() {
+        let vc = ViewController()
+        
+        // ダミーのデータを設定する
+        let testItems = ["test-item-1", "test-item-2"]
+        vc.items = testItems
+        
+        let rowCount = vc.tableView(vc.tableView, numberOfRowsInSection: 1)
+        XCTAssertEqual(rowCount, testItems.count)
+    }
+    
+    func test_Cellのテキストに期待されたラベルが表示されていること() {
+        let vc = ViewController()
+        
+        let testItems = ["test-item-1"]
+        vc.items = testItems
+        
+        vc.tableView.cellForRow(at: IndexPath(row: 0, section: 1))
+        let cell = vc.tableView(vc.tableView, cellForRowAt: IndexPath(row: 0, section: 1))
+        XCTAssertEqual(cell.textLabel?.text, "test-item-1")
+    }
+}
